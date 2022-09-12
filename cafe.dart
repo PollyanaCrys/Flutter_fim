33c61b05f959388ccb5bc4ccbe1da4d515637393
PollyanaCrys <103380013+PollyanaCrys@users.noreply.github.com>
2022-09-12 12:24:12 -0300

    Create cafe.dart

---
+++ cafe.dart
@@ -0,0 +1,18 @@
+class Cafe {
+  final int id;
+  final String foto;
+  final String nome;
+  final String aux;
+  final String volume;
+  final String preco;
+  final String ingredientes;
+
+  Cafe(
+      {required this.id,
+      required this.foto,
+      required this.nome,
+      required this.aux,
+      required this.volume,
+      required this.preco,
+      required this.ingredientes});
+}
